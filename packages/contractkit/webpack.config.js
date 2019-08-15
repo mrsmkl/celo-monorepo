0ed5935d173c4ec89a492406413c01fedf238d4f
Martin Chrzanowski <martin@celo.org>
2019-08-15 15:23:04 -0700

    Add webpack

---
+++ packages/contractkit/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const TsconfPathPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    plugins: [new TsconfPathPlugin()],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
