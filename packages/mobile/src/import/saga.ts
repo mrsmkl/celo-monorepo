530395045498a6366b48a3827e3cb4f2edfb5ff9
Rohit <8ohit.dua@gmail.com>
2019-09-11 15:02:32 +0200

    comment out import wallet with zero money notify code

--- packages/mobile/src/import/saga.ts
+++ packages/mobile/src/import/saga.ts
@@ -1,5 +1,5 @@
-import { getStableTokenContract } from '@celo/walletkit'
-import BigNumber from 'bignumber.js'
+// import { getStableTokenContract } from '@celo/walletkit'
+// import BigNumber from 'bignumber.js'
 import { mnemonicToSeedHex } from 'react-native-bip39'
 import { call, put, spawn, takeLeading } from 'redux-saga/effects'
 import { setBackupCompleted } from 'src/account'
@@ -11,7 +11,7 @@ import { redeemComplete } from 'src/invite/actions'
 import { navigateReset } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { setKey } from 'src/utils/keyStore'
-import { web3 } from 'src/web3/contracts'
+// import { web3 } from 'src/web3/contracts'
 import { assignAccountFromPrivateKey } from 'src/web3/saga'
 
 export function* importBackupPhraseSaga(action: ImportBackupPhraseAction) {
@@ -24,11 +24,12 @@ export function* importBackupPhraseSaga(action: ImportBackupPhraseAction) {
     yield put(redeemComplete(true))
     yield put(refreshAllBalances())
     navigateReset(Screens.ImportContacts)
-    const StableToken = yield call(getStableTokenContract, web3)
-    const accountBalance = new BigNumber(yield call(StableToken.methods.balanceOf(account).call))
-    if (accountBalance.isZero()) {
-      yield put(showError(ErrorMessages.BACKUP_ZERO_BAL_MESSAGE))
-    }
+    // TODO:
+    // const StableToken = yield call(getStableTokenContract, web3)
+    // const accountBalance = new BigNumber(yield call(StableToken.methods.balanceOf(account).call))
+    // if (accountBalance.isZero()) {
+    //   yield put(showError(ErrorMessages.BACKUP_ZERO_BAL_MESSAGE))
+    // }
   } else {
     yield put(showError(ErrorMessages.IMPORT_BACKUP_FAILED))
   }
