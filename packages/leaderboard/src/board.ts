3434407e63eb96d77add296ad0ee03bb765b447d
Sami Mäkelä <mrsmkl@gmail.com>
2019-12-05 18:06:55 +0200

    wrote something to the spreadsheet

--- packages/leaderboard/src/board.ts
+++ packages/leaderboard/src/board.ts
@@ -1,115 +1,20 @@
-import { ContractKit, newKitFromWeb3, CeloContract } from '@celo/contractkit'
-import { AccountsWrapper } from '@celo/contractkit/lib/wrappers/Accounts'
-import Web3 from 'web3'
-import http from 'http'
 import fs from 'fs'
 import readline from 'readline'
 import { google } from 'googleapis'
-import { Client } from 'pg'
-import { verifyClaim, Claim } from '@celo/contractkit/lib/identity/claims/claim'
-import { ClaimTypes } from '@celo/contractkit/lib/identity'
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = 'token.json'
 
-function addressToBinary(a: string) {
-  try {
-    if (a.substr(0, 2) == '0x') return a.substr(2)
-    else return a
-  } catch (_err) {
-    return a
-  }
-}
-
-const LEADERBOARD_DATABASE = process.env['LEADERBOARD_DATABASE'] || 'blockscout'
 const LEADERBOARD_TOKEN = process.env['LEADERBOARD_TOKEN'] || 0
 const LEADERBOARD_SHEET =
   process.env['LEADERBOARD_SHEET'] || '1d3pZNof8p3z8M9O3MH5FZG5dA3e-L52XiJ4qA5o7X0Y'
-const LEADERBOARD_WEB3 = process.env['LEADERBOARD_WEB3'] || 'http://localhost:8545'
 
 async function updateDB(lst: any[][]) {
-  const client = new Client({ database: LEADERBOARD_DATABASE })
-  await client.connect()
-  const res = await client.query(
-    'INSERT INTO competitors (address, multiplier)' +
-      " SELECT decode(m.address, 'hex') AS address, m.multiplier FROM json_populate_recordset(null::json_type, $1) AS m" +
-      ' ON CONFLICT (address) DO UPDATE SET multiplier = EXCLUDED.multiplier RETURNING *',
-    [
-      JSON.stringify(
-        lst.map((a) => {
-          return { address: addressToBinary(a[0]), multiplier: a[1] }
-        })
-      ),
-    ]
-  )
-  console.log(res.rows)
-  await client.end()
-  await readAssoc(lst.map((a) => a[0].toString()))
-}
-
-async function updateRate(kit: ContractKit) {
-  const client = new Client({ database: LEADERBOARD_DATABASE })
-  await client.connect()
-  const token = await kit.contracts.getStableToken()
-  const oracle = await kit.contracts.getSortedOracles()
-  const rate = await oracle.medianRate(CeloContract.StableToken)
-
-  console.log(token.address)
-
-  const res = await client.query(
-    'INSERT INTO exchange_rates (token, rate) VALUES ($1, $2)' +
-      ' ON CONFLICT (token) DO UPDATE SET rate = EXCLUDED.rate RETURNING *',
-    [Buffer.from(token.address.substr(2), 'hex'), rate.rate.toNumber()]
-  )
-  console.log(res.rows)
-  await client.end()
-}
-
-async function processClaims(kit: ContractKit, address: string, data: string) {
-  try {
-    console.log('process claim', address, data)
-    const info: any = JSON.parse(data)
-    const orig_lst: any[] = info.claims
-    const lst: string[] = []
-    const accounts = await kit.contracts.getAccounts()
-    for (let i = 0; i < orig_lst.length; i++) {
-      console.log('account claim', orig_lst[i])
-      let claim: Claim = {
-        type: ClaimTypes.ACCOUNT,
-        timestamp: orig_lst[i].timestamp,
-        address: orig_lst[i].address,
-        publicKey: undefined,
-      }
-      const status = await verifyClaim(claim, address, accounts.getMetadataURL)
-      if (status) console.error(status)
-      else lst.push(claim.address)
-    }
-    lst.push(address)
-    const client = new Client({ database: LEADERBOARD_DATABASE })
-    await client.connect()
-    const res = await client.query(
-      'INSERT INTO claims (address, claimed_address)' +
-        " SELECT decode(m.address,'hex'), decode(m.claimed_address,'hex') FROM json_populate_recordset(null::json_assoc, $1) AS m" +
-        ' ON CONFLICT (address, claimed_address) DO NOTHING RETURNING *',
-      [
-        JSON.stringify(
-          lst.map((a) => {
-            const res = { address: addressToBinary(address), claimed_address: addressToBinary(a) }
-            console.log(res)
-            return res
-          })
-        ),
-      ]
-    )
-    console.log(res.rows)
-    await client.end()
-  } catch (err) {
-    console.error('Cannot process claims', err)
-  }
+  console.log('Found stuff', lst)
 }
 
 function getCredentials() {
@@ -199,30 +104,18 @@ function getInfo(auth: any) {
       }
     }
   )
-}
-
-async function readAssoc(lst: string[]) {
-  const web3 = new Web3(LEADERBOARD_WEB3)
-  const kit: ContractKit = newKitFromWeb3(web3)
-  updateRate(kit)
-  const accounts: AccountsWrapper = await kit.contracts.getAccounts()
-  function getFromUrl(a: string, url: string) {
-    http
-      .get(url, (resp) => {
-        let data = ''
-        resp.on('data', (chunk) => (data += chunk))
-        resp.on('end', () => processClaims(kit, a, data))
-      })
-      .on('error', (err) => console.log('Error: ' + err.message))
+  let req = {
+    spreadsheetId: LEADERBOARD_SHEET,
+    range: 'Sheet1!D2',
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      range: 'Sheet1!D2',
+      majorDimension: 'ROWS',
+      values: [['blaahs']],
+    },
   }
-  lst.forEach(async (a) => {
-    try {
-      const url = await accounts.getMetadataURL(a)
-      if (url == '') processClaims(kit, a, '{"claims": []}')
-      else getFromUrl(a, url)
-    } catch (err) {
-      console.error('Bad address', a, err.toString())
-    }
+  sheets.spreadsheets.values.update(req, (err: any, res: any) => {
+    console.log(res, err)
   })
 }
 
